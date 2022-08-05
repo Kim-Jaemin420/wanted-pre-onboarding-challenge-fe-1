8103b454bdbc60e6efe9083d898a69ad237eb629
Kim-Jaemin420 <jany240@naver.com>
2022-08-05 16:39:58 +0900

    Feat: 이메일과 비밀번호를 조건에 맞게 validate

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -5,20 +5,42 @@ function Auth() {
   const [password, setPassword] = useState('');
 
   const handleChangeEmail = e => {
-    console.log(e.target.value);
     setEmail(e.target.value);
   };
 
   const handleChangePassword = e => {
-    console.log(e.target.value);
     setPassword(e.target.value);
+  };
+
+  const validateEmailCondition = email => {
+    const EMAIL_REGEX =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+    if (EMAIL_REGEX.test(email)) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const validatePasswordCondition = password => {
+    const PASSWORD_MINIMUM_LENGTH = 8;
+
+    if (password.length >= PASSWORD_MINIMUM_LENGTH) {
+      return true;
+    }
+
+    return false;
   };
 
   return (
     <form>
       <input type="email" required value={email} onChange={handleChangeEmail} />
       <input type="password" required value={password} onChange={handleChangePassword} />
-      <input type="submit" disabled={true} />
+      <input
+        type="submit"
+        disabled={!(validateEmailCondition(email) && validatePasswordCondition(password))}
+      />
     </form>
   );
 }
