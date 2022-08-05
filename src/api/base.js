8f003d31de6cf016c07806e98d76483157010fcf
Kim-Jaemin420 <jany240@naver.com>
2022-08-05 18:02:16 +0900

    Feat: axios instance를 만드는 base 코드 작성

---
+++ src/api/base.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { ACCESS_TOKEN, BASE_URL, HTTP_METHODS } from '../consts/net.js';
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+const handleRequest = config => {
+  const TOKEN = localStorage.getItem(ACCESS_TOKEN);
+
+  return TOKEN
+    ? {
+        ...config,
+        headers: {
+          ...config.headers,
+          Authorization: `${TOKEN}`,
+        },
+      }
+    : config;
+};
+
+const handleRespone = response => {
+  return response.data;
+};
+
+const handleError = error => {
+  throw error;
+};
+
+const createApiMethod = (axiosInstance, methodType) => config => {
+  return axiosInstance({ ...handleRequest(config), method: methodType })
+    .then(handleRespone)
+    .catch(handleError);
+};
+
+export default {
+  get: createApiMethod(axiosInstance, HTTP_METHODS.GET),
+  post: createApiMethod(axiosInstance, HTTP_METHODS.POST),
+  put: createApiMethod(axiosInstance, HTTP_METHODS.PUT),
+  delete: createApiMethod(axiosInstance, HTTP_METHODS.DELETE),
+};
