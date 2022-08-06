5f32d3f0b3add655c4b3bbdcdc6b9de5f7fa77b0
Kim-Jaemin420 <jany240@naver.com>
2022-08-06 22:14:53 +0900

    Feat: route 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Auth, Home } from './pages/index.js';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Auth, TodoList, TodoDetails } from './pages/index.js';
+import './App.css';
+import { getLocalStorage } from './utils/common.js';
+import { ACCESS_TOKEN } from './consts/net.js';
 
 function App() {
+  const TOKEN = getLocalStorage(ACCESS_TOKEN);
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/auth" element={<Auth />}></Route>
+          <Route path="/" element={<TodoList />} />
+          <Route path="/signUp" element={<Auth />} />
+          <Route path="/login" element={<Auth />} />
+          <Route path="/todos" element={<TodoList />} />
+          <Route path="/todos/*" element={<TodoDetails />} />
         </Routes>
       </BrowserRouter>
     </div>
