5ae006231db73e7a3c92d8358deb0adb0074e44f
Kim-Jaemin420 <jany240@naver.com>
2022-08-06 22:18:22 +0900

    Feat: 로그인과 회원가입을 path이름으로 구분

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
-import { signUp } from '../api/user';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { signUp, login } from '../api/user';
+import { ACCESS_TOKEN } from '../consts/net';
+import { setLocalStorage } from '../utils/common';
 
 function Auth() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isSignUpPage = location.pathname === '/signUp';
+  const isLoginPage = location.pathname === '/login';
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -34,9 +41,29 @@ function Auth() {
     return false;
   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    signUp({ email, password });
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    if (isSignUpPage) {
+      try {
+        const { message } = await signUp({ email, password });
+        alert(message);
+        navigate('/login');
+      } catch (error) {
+        alert('이미 가입된 회원입니다');
+      }
+    }
+
+    if (isLoginPage) {
+      try {
+        const { token } = await login({ email, password });
+        setLocalStorage(ACCESS_TOKEN, token);
+        navigate('/');
+      } catch (error) {
+        console.log(error);
+        alert('이메일 혹은 비밀번호가 일치하지 않습니다.');
+      }
+    }
   };
 
   return (
