317c578793e99759685b5fabb7a69c7401b19090
Kim-Jaemin420 <jany240@naver.com>
2022-08-06 22:17:42 +0900

    Feat: 네트워크 상수 선언

---
+++ src/consts/net.js
@@ -0,0 +1,11 @@
+export const HTTP_METHODS = {
+  GET: 'get',
+  POST: 'post',
+  PATCH: 'patch',
+  PUT: 'put',
+  DELETE: 'delete',
+};
+
+export const ACCESS_TOKEN = 'ACCESS_TOKEN';
+
+export const BASE_URL = '/api';
